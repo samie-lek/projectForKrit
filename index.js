b14e335ef291c1ebb46e28fac42bd6f26ae0725e
samie-lek <83439649+samie-lek@users.noreply.github.com>
2024-01-01 22:06:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".t83.tank").click(function () {
+    $(".t83.vid").css("display", "block");
+    $(".video").css("display", "flex");
+})
+
+$(".exit-vid").click(function () {
+    $(".t83.vid").css("display", "none");
+    $(".video").css("display", "none");
+})
